cd885f16ec560766d5187b13d271e47c0238c850
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-04-24 22:45:34 -0600

    6th commit to heroku now using node pg

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const session = require('express-session')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // const config = require('./config');
+
+
+
+
+
 var pg = require('pg');
 pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
+
+
+
 const massive = require('massive');
-let connectionString = "postgres://ryanbryce@localhost/bearbones";
+let connectionString = process.env.DATABASE_URL;
 let massiveInstance = massive.connectSync({connectionString})
 var stripe = require('stripe')(process.env.STRIPESECRETT);
 console.log(process.env);
